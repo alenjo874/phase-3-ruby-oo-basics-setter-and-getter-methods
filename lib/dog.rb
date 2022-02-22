1105a97bb333a1ed981fd87e9fa79019d5c5ef69
Alen Jo <alenjo874@gmail.com>
2022-02-22 14:11:03 -0500

    commit tree 15942f165842ed26796da862fbacba5c61bbd1a8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,43 @@
 class Dog
+     # setter method
+  def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # getter method
+  def breed
+    @breed
+  end
 end
+
+class Person
+
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+
+    def job=(job)
+        @job = job
+      end
+    
+      # getter method
+      def job
+        @job
+      end
+  
+  end
